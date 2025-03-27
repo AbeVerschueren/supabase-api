d326c3df00d528d5eadd13e80e40c9c14697ae8e
AbeVerschueren <153118736+AbeVerschueren@users.noreply.github.com>
2025-03-27 02:31:16 +0100

    test

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ import dotenv from 'dotenv';
 import { createClient } from '@supabase/supabase-js';
 
 dotenv.config();
-app.use(express.static('public'));
+
 
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(express.static('public'));
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
