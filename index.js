d8ac7a003b1faf087aa7b3381de8c6207b11657b
AbeVerschueren <153118736+AbeVerschueren@users.noreply.github.com>
2025-03-27 02:25:20 +0100

    add-minifig toevoegen

--- index.js
+++ index.js
@@ -24,6 +24,30 @@ app.get('/minifigs', async (req, res) => {
   res.json(data);
 });
 
+app.use(express.json()); // nodig om JSON body te lezen
+
+// data naar minifigs table te sturen 
+app.post('/minifigs', async (req, res) => {
+  const { name, rarity, img, auth } = req.body;
+
+  if (auth !== process.env.API_SECRET) {
+    return res.status(401).json({ error: 'Niet geautoriseerd' });
+  }
+
+  const { data, error } = await supabase
+    .from('minifigs')
+    .insert([{ name, rarity, img }]);
+
+  if (error) {
+    return res.status(500).json({ error: error.message });
+  }
+
+  res.status(201).json({ success: true, data });
+});
+
+
+
+
 app.listen(port, () => {
-  console.log(`API runnning on http://localhost:${port}`);
+  console.log(`API runnning on`);
 });
